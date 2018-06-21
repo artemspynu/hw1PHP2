0edd91d2968321b8889bbdd80aedbfdd13bfaea6
Artem <artem-lider@mail.ru>
2018-06-22 02:02:36 +0300

    добавлены 5,6,7 задания

--- app.php
+++ app.php
@@ -26,3 +26,74 @@ $a->count = 1;
 $a->price = 3200;
 /*$a->totalPrice = count * price;*/
 $a->calcPrice();
+
+
+// Задание 5
+
+class A
+{
+    public function foo()
+    {
+        static $x = 0;
+        echo ++$x;
+    }
+}
+
+$a1 = new A();
+$a2 = new A();
+$a1->foo();
+$a2->foo();
+$a1->foo();
+$a2->foo();
+
+//1234, $x увеличивается на 1.
+
+
+// Задание 6
+
+class A
+{
+    public function foo()
+    {
+        static $x = 0;
+        echo ++$x;
+    }
+}
+
+class B extends A
+{
+}
+
+$a1 = new A();
+$b1 = new B();
+$a1->foo();
+$b1->foo();
+$a1->foo();
+$b1->foo();
+
+//1122 сначала происходит наследование, потом увеличение на 1.
+
+
+//Задание 7
+
+class A
+{
+    public function foo()
+    {
+        static $x = 0;
+        echo ++$x;
+    }
+}
+
+class B extends A
+{
+}
+
+$a1 = new A;
+$b1 = new B;
+$a1->foo();
+$b1->foo();
+$a1->foo();
+$b1->foo();
+
+//1122
